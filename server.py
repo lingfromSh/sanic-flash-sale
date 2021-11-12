dde03289e0a796f146e87aaa7e54466bdce3e66d
StephenLing <lingfromsh@163.com>
2021-11-12 10:10:10 +0800

    Update server.py

--- server.py
+++ server.py
@@ -1,12 +1,31 @@
-import os
+import argparse
+
 from sanic import Sanic
+from sanic.config import Config
 from sanic.response import HTTPResponse
-from common.settings import YamlConfig
+from tortoise.contrib.sanic import register_tortoise
 
-config = YamlConfig(os.environ.get("SANIC_CONFIGURATION_PATH"))
-application = Sanic(config.PROJECT.NAME, config=config)
+# Collect command line args
+parser = argparse.ArgumentParser()
+parser.add_argument('-m', '--mode', default='dev', help='Which mode to start with?')
+args = parser.parse_args()
+
+# Collect config
+config = Config()
+config.update_config(f"settings/{args.mode}.py")
+
+# Init sanic app
+application = Sanic('sanic-flash-sale', config=config, strict_slashes=True)
+
+# Register tortoise for sanic.
+register_tortoise(application, config=application.config.POSTGRES, generate_schemas=True)
 
 
-@application.get("/health-check/", strict_slashes=True)
+# Register health check url
+@application.get("/health-check/")
 async def health_check(request):
     return HTTPResponse("ok", content_type="application/json")
+
+
+if __name__ == "__main__":
+    application.run(host='0.0.0.0', port=8000, access_log=True)
